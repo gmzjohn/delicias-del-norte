8b388920a39d470998743ef753f9834978747ce7
John GÃomez <gmzjohn7@gmail.com>
2025-12-23 18:03:50 -0400

    Create OrderItem entity
    Define OrderItem class with core properties

---
+++ app/domains/order_item.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from decimal import Decimal
+from .base_entity import BaseEntity
+from .enums.entity_status import EntityStatus
+from .product import Product
+
+class OrderItem(BaseEntity):
+    def __init__(
+        self,
+        id: int,
+        status: EntityStatus,
+        created_by: int,
+        updated_by: int,
+        created_at: datetime,
+        updated_at: datetime,
+        product: Product,
+        quantity: int,
+        price: Decimal,
+    ):
+        super().__init__(
+            id=id,
+            status=status,
+            created_by=created_by,
+            updated_by=updated_by,
+            created_at=created_at,
+            updated_at=updated_at,
+        )
+        
+        self.product = product
+        self.quantity = quantity
+        self.price = price
