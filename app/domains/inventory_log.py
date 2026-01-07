cf95ab4139b61da03214f94eeec0a9a73187af04
John Gomez <gmzjohn7@gmail.com>
2026-01-07 11:47:10 -0400

    Create InventoryLog entity
    Define InventoryLog class with core properties.

---
+++ app/domains/inventory_log.py
@@ -0,0 +1,29 @@
+from datetime import datetime
+from product import Product
+from ingredients import Ingredient
+from base_entity import BaseEntity
+from enums.entity_status import EntityStatus
+
+class InventoryLog(BaseEntity):
+    def __init__(self,
+                 id: int,
+                 status: EntityStatus,
+                 created_by: int,
+                 updated_by: int,
+                 created_at: datetime,
+                 updated_at: datetime,
+                 ingredient: Ingredient,
+                 product: Product,
+                 quantity: int,
+    ):
+        super().__init__(
+            id = id,
+            status = status,
+            created_by = created_by,
+            updated_by = updated_by,
+            created_at = created_at,
+            updated_at = updated_at
+        )
+        self.ingredient = ingredient
+        self.product = product
+        self.quantity = quantity
